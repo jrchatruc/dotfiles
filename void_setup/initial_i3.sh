4ebf9fd67775c787852c5ab46473afc8b4d1a0eb
Javier Chatruc <jrchatruc@gmail.com>
2024-03-25 14:43:32 -0300

    Fix

--- void_setup/initial_i3.sh
+++ void_setup/initial_i3.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-xbps-install -Su
-xbps-install -y xorg i3 i3status dbus lightdm lightdm-gtk-greeter alacritty elogind
+sudo xbps-install -Su
+sudo xbps-install -y xorg i3 i3status dbus lightdm lightdm-gtk-greeter alacritty elogind
 sudo ln -s /etc/sv/dbus /var/service
 sudo ln -s /etc/sv/lightdm /var/service
 sudo ln -s /etc/sv/elogind /var/service
