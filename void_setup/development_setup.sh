ef6c897b072d6898117eb9104a14b0c764934260
Javier Chatruc <jrchatruc@gmail.com>
2024-03-25 14:32:15 -0300

    Fixes

--- void_setup/development_setup.sh
+++ void_setup/development_setup.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
 set -e
-sudo -i
 
-xbps-install -y base-devel git zsh
+sudo xbps-install -y base-devel git zsh
 chsh -s $(which zsh)
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-git clone https://github.com/so-fancy/diff-so-fancy.git "~/.config/"
+git clone https://github.com/so-fancy/diff-so-fancy.git ~/.config/diff-so-fancy
 
-xbps-install -y eza neofetch neovim ripgrep fzf ctags direnv
+sudo xbps-install -y eza neofetch neovim ripgrep fzf ctags direnv
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir ~/.config/nvim
+mkdir -p ~/.config/nvim
 
-xbps-install -y vim firefox alsa-utils alsa-firmware apulse sof-firmware sof-tools
+sudo xbps-install -y vim firefox alsa-utils alsa-firmware apulse sof-firmware sof-tools
 sudo ln -s /etc/sv/alsa /var/service
 
 git clone https://github.com/jrchatruc/dotfiles.git
-cp dotfiles/config/{.gitconfig,.gitignore_global,.psqlrc,.vimrc,zpreztorc,zshrc} ~/
-cp dotfiles/config/aliases.zsh ~/.config/
-cp dotfiles/config/nvim/init.vim ~/.config/nvim/
+cp ../config/{.gitconfig,.gitignore_global,.psqlrc,.vimrc,.zpreztorc,.zshrc} ~/
+cp ../config/aliases.zsh ~/.config/
+cp ../config/nvim/init.vim ~/.config/nvim/
 
 cp -f i3status.conf /etc/i3status.conf
 cp -f config ~/.config/i3/config
