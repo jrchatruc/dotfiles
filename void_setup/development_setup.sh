60d60c45b9caf534dfbbc463df34408da5a9dd99
Javier Chatruc <jrchatruc@gmail.com>
2024-03-25 18:50:17 -0300

    Add GPU stuff to installed packages

--- void_setup/development_setup.sh
+++ void_setup/development_setup.sh
@@ -14,7 +14,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim
 
-sudo xbps-install -y vim firefox alsa-utils alsa-firmware apulse pulseaudio sof-firmware sof-tools
+sudo xbps-install -y vim firefox alsa-utils alsa-firmware apulse pulseaudio sof-firmware sof-tools mesa-vulkan-intel intel-video-accel
 sudo ln -s /etc/sv/alsa /var/service
 
 cp ../config/{.gitconfig,.gitignore_global,.psqlrc,.vimrc,.zpreztorc,.zshrc} ~/
