bd342bd5a19a341e0e36d72769e6a5d7a4ba26be
Javier Chatruc <jrchatruc@gmail.com>
2024-05-25 09:09:08 -0300

    exa -> eza

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -5,7 +5,7 @@
 # $> git init --bare $HOME/.dotfiles.git
 # $> dotfiles config --local status.showUntrackedFiles no
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
-alias ls='exa'
+alias ls='eza'
 
 # git
 alias g='git'
